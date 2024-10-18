39eebe2f6b2976fad65b42b6c9ceda3c87dd1bfa
Fernando Greca <144340988+FernandoGreca@users.noreply.github.com>
2024-10-18 15:21:38 -0300

    Edição do card Prato | Não é possível adicionar prato sem imagem (proposital)

--- hamburgueria/src/main/java/com/fernandoproject/hamburgueria/controller/HomeController.java
+++ hamburgueria/src/main/java/com/fernandoproject/hamburgueria/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fernandoproject.hamburgueria.dao.PratoDao;
 import com.fernandoproject.hamburgueria.model.Prato;
@@ -24,7 +25,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-
 @Controller
 public class HomeController {
 
@@ -32,7 +32,6 @@ public class HomeController {
 
     @Autowired
     private PratoDao pratoRepositorio;
-    
 
     @GetMapping("/")
     public ModelAndView index() {
@@ -53,7 +52,7 @@ public class HomeController {
 
         mv.addObject("listaPratos", pratos);
         mv.setViewName("pages/cardapio");
-        
+
         return mv;
     }
 
@@ -65,33 +64,76 @@ public class HomeController {
         return mv;
     }
 
+    @SuppressWarnings("null")
     @GetMapping("/cardapio/mostrarImagem/{imagem}")
     @ResponseBody
-    public byte[] retornarImagem(@PathVariable("imagem") String imagem ) throws IOException {
+    public byte[] retornarImagem(@PathVariable("imagem") String imagem) throws IOException {
         File imagemArquivo = new File(caminhoImagens + imagem);
         if (imagem != null || imagem.trim().length() > 0) {
             return Files.readAllBytes(imagemArquivo.toPath());
         }
         return null;
     }
-    
 
     @PostMapping("AdicionarPrato")
-    public ModelAndView adicionarPrato(@Valid Prato prato, BindingResult br, @RequestParam("file") MultipartFile arquivo) {
+    public ModelAndView adicionarPrato(@Valid Prato prato, BindingResult br,
+            @RequestParam("file") MultipartFile arquivo) {
         ModelAndView mv = new ModelAndView();
 
         if (br.hasErrors()) {
             mv.setViewName("pages/adicionar-prato");
             mv.addObject("prato");
-            return mv;
-        } 
+        } else if (arquivo.isEmpty()) {
+            mv.addObject("msgErroAdicionarPrato", "Não é possível adicionar um prato sem foto.");
+            mv.setViewName("pages/adicionar-prato");
+        } else {
+            salvarImagemPrato(arquivo, prato, mv);
+        }
+
+        return mv;
+    }
+
+    @GetMapping("/editar/{id}")
+    public ModelAndView editar(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("pages/editar");
+        Prato prato = pratoRepositorio.getReferenceById(id);
+        mv.addObject("prato", prato);
+
+        return mv;
+    }
+
+    @PostMapping("editar")
+    public ModelAndView editar(Prato prato, RedirectAttributes redirectAttributes,
+            @RequestParam("file") MultipartFile arquivo) {
+        ModelAndView mv = new ModelAndView();
+
+        // Recupera o prato existente no banco de dados para comparar a imagem
+        Prato pratoExistente = pratoRepositorio.getReferenceById(prato.getId());
+
+        if (!arquivo.isEmpty()) {
+            // Se o arquivo não está vazio, chama o método para salvar a imagem junto com as alterações
+            salvarImagemPrato(arquivo, prato, mv);
+            mv.setViewName("redirect:/cardapio");
+        } else {
+            // Se o arquivo está vazio, mantém a imagem já existente e realiza as alterações
+            prato.setNomeImagem(pratoExistente.getNomeImagem());
+            pratoRepositorio.save(prato);
+            mv.setViewName("redirect:/cardapio");
+        }
+
+        return mv;
+    }
+
+    public void salvarImagemPrato(@RequestParam("file") MultipartFile arquivo, @Valid Prato prato, ModelAndView mv) {
 
         pratoRepositorio.save(prato);
 
         try {
             if (!arquivo.isEmpty()) {
                 byte[] bytes = arquivo.getBytes();
-                Path caminho = Paths.get(caminhoImagens + String.valueOf(prato.getId()) + arquivo.getOriginalFilename());
+                Path caminho = Paths
+                        .get(caminhoImagens + String.valueOf(prato.getId()) + arquivo.getOriginalFilename());
                 Files.write(caminho, bytes);
 
                 prato.setNomeImagem(String.valueOf(prato.getId()) + arquivo.getOriginalFilename());
@@ -101,7 +143,6 @@ public class HomeController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        return mv;
+
     }
 }
